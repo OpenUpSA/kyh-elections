1148265aa82862429e6401f0efd31f32fd111648
Adi Eyal <adi@burgercom.co.za>
2013-12-08 10:54:14 +0200

    Fixed fabfile config

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from fabric import api
 api.env.hosts = ["adi@code4sa.org:2222"]
 
 code_dir = "/var/www/hood.code4sa.org"
-env_dir = "/home/adi/.virtualenvs/a2w"
+env_dir = "/home/adi/.virtualenvs/hood"
 python = "%s/bin/python" % env_dir
 pip = "%s/bin/pip" % env_dir
