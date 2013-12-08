5d7ae473cfa3d311ccbad8aaf979562706b2cd7f
Adi Eyal <adi@burgercom.co.za>
2013-12-08 09:45:43 +0200

    Fixed deployment bug

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from fabric import api
 
 api.env.hosts = ["adi@code4sa.org:2222"]
 
-code_dir = "/var/www/wards.code4sa.org"
+code_dir = "/var/www/hood.code4sa.org"
 env_dir = "/home/adi/.virtualenvs/a2w"
 python = "%s/bin/python" % env_dir
 pip = "%s/bin/pip" % env_dir
