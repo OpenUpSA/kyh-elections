2bc131336cb85a4859594052305a871ee48a9e34
Adi Eyal <adi@burgercom.co.za>
2013-12-09 09:21:31 +0200

    Forgot to import track in web.py

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import votes
+from track import track
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
